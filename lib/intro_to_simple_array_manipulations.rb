1aca499a02d7de2256dd77f3ac910dcce45014f6
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-16 22:46:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
-def using_push(countries, " ")
-  countries = []
-  countries.push
+def using_push(countries_in_western_africa, next_country)
+  countries = [["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]]
+  countries.push["Niger"]
   puts countries
 end
